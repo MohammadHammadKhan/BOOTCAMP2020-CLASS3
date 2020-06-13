646f7b3df6bc3cad4ad1c10b54526e4079ec1fe4
Mohammad Hammad Khan <66447399+MHKDESIGNS@users.noreply.github.com>
2020-06-14 00:35:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        HELLO WORLD
         <a
           className="App-link"
           href="https://reactjs.org"
